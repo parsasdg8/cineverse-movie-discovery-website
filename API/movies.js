b4acf90530ec28f27776bb596289ee126bcaebb0
Parsa Sadeghi <p4rsasadeghi@gmail.com>
2026-09-02 23:31:29 +0330

    Update movies.js

--- API/movies.js
+++ API/movies.js
@@ -14,9 +14,13 @@ export default async function handler(req, res) {
 
         const data = await response.json();
 
-        res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=86400");
+        res.setHeader(
+            "Cache-Control",
+            "public, s-maxage=3600, stale-while-revalidate=86400"
+        );
 
         return res.status(200).json(data);
+
     } catch (error) {
         return res.status(500).json({
             error: "Unable to fetch movies",
